77e1a581873495f937d1e36d9b0a1fe865ee27a8
ashlinhanson <ashlinhanson@gmail.com>
2020-10-07 15:08:39 -0500

    added pseudo code for depts js file

---
+++ department.js
@@ -0,0 +1,18 @@
+function addDept(){
+    //function for adding a dept
+};
+
+function viewDept(){
+    //function for viewing the departments
+};
+
+
+
+//eventually will need :
+
+//Update departments
+
+//Delete departments
+
+
+module.exports = "department"
