1526805ffbe2badafaa899f116199177fc5d2a93
ashlinhanson <ashlinhanson@gmail.com>
2020-10-10 15:44:22 -0500

    added more to seed and edited format

--- schema.sql
+++ schema.sql
@@ -4,22 +4,22 @@ CREATE DATABASE employeetracking_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30),
-    PRIMARY KEY (id)
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id) 
 );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL(6, 2),
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(6, 2) NOT NULL,
     department_id INT NOT NULL
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT(5) NOT NULL,
-    manager_id INT(5)
+    manager_id INT(5) NOT NULL
 
 );
--- seed.sql
+++ seed.sql
@@ -1,16 +1,23 @@
-INSERT INTO department (id, name)
+INSERT INTO department (name)
 
-VALUES (1, "Sales"), (2, "Customer Service"), (3, "Accounting"), (4, "Quality Assuarance"), (5, "Other Department");
+VALUES ("Sales"), ("Customer Service"), ("Accounting"), ("Quality Assuarance"), ("Misc Department");
 
-INSERT INTO role (id, title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 
-VALUES (10, "Branch Manager", 75,000.00, 5),
-(11, "Salesperson", 35,000.00, 1),
-(12, "Accountant", 45,000.00, 3),
-(13, "Customer Service Representative", 38,000.00, 2),
-(14, "Quality Assurance Agent", 38,000.00, 4),
-(15, "Receptionist", 35,000.00, 5),
-(16, "Temp Employee", 20,000.00, 5),
-(17, "Human Resources Representative", 50,000.00, 5);
+VALUES ("Branch Manager", 75,000.00, 5),
+("Salesperson", 35,000.00, 1),
+("Accountant", 45,000.00, 3),
+("Customer Service Representative", 38,000.00, 2),
+("Quality Assurance Agent", 38,000.00, 4),
+("Receptionist", 35,000.00, 5),
+("Temp Employee", 20,000.00, 5),
+("Human Resources Representative", 50,000.00, 5);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+
+VALUES ("Michael", "Scott"), ("Dwight", "Shrute"), ("Jim", "Halpert"), ("Pam", "Beesly"),
+("Kelly", "Kapoor"), ("Ryan", "Howard"), ("Creed", "Bratton"), ("Meredith", "Palmer"), 
+("Toby", "Flenderson"), ("Angela", "Martin"), ("Phyllis", "Vance"), ("Oscar", "Martinez"), 
+("Kevin", "Malone"), ("Stanley", "Hudson");
+
+
