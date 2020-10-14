937fb8d2bf9b9b31494aaad56dad76ed2235268f
ashlinhanson <ashlinhanson@gmail.com>
2020-10-14 15:53:07 -0500

    consolidating code

--- employeeTracking.js
+++ employeeTracking.js
@@ -78,3 +78,117 @@ function startApp(){
         }
     })
 };
+
+function addRole(){
+    //function to add a role
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is the name of the role being added?",
+            name: "roleName"
+        },
+    ]).then(answer => {
+
+        connection.query(
+            "INSERT INTO role SET ?",
+            {
+                name: answer.roleName,
+            }, function(err, results){
+                if (err) throw err;
+                console.log("The role was added successfully!");
+            }
+        )
+    });
+};
+
+function viewRoles(){
+    //function to view roles
+    connection.query(
+        "SELECT * FROM role",
+        function(err, results){
+            if (err) throw err;
+            console.log(results);
+        }
+    )
+
+};
+
+function updateEmpRole(){
+    // update employee role function
+};
+
+function addEmployee(){
+    //function to add an employee
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is the new employees first name?",
+            name: firstName
+        },
+        {
+            type: "input",
+            message: "What is the new employees last name?",
+            name: "lastName"
+        },
+    ]).then(answer => {
+        
+        connection.query(
+            {
+                first_name: answer.firstName,
+                last_name: answer.lastName
+            }, function (err, result){
+                if (err) throw err;
+                console.log("The employee has been added successfully")
+            }
+        )
+    })
+};
+
+function viewEmployees(){
+    //function to view employees
+    connection.query("SELECT * FROM department", function(err, results){
+        if (err) throw err;
+        console.log(results);
+    });
+
+};
+
+function addDept(){
+    //function for adding a dept
+    inquire
+    .prompt([
+        {
+            name: "deptName",
+            type: "input",
+            message: "What is the name of the department you are adding",
+            validate: function(value){
+                if (isNaN(value) === false){
+                    return true;
+                }
+                return false;
+            }
+        },
+    ]).then(function(answer){
+        connection.query(
+            "INSERT INTO department SET ?",
+            {
+                name: answer.deptName
+            },
+            function (err) {
+                if (err) throw err;
+                console.log("Your department has been added successfully");
+                addDept();
+            }
+        );
+    });
+};
+
+function viewDept(){
+    //function for viewing the departments
+    connection.query("SELECT * FROM department", function(err, results){
+        if (err) throw err;
+        console.log(results);
+})};
+
